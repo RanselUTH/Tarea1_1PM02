2ea223ab70660f27edf00770045b98da5c989a7f
YovannyUTH <107007204+YovannyUTH@users.noreply.github.com>
2022-10-21 23:32:43 -0600

    Agregar archivos de proyecto.

---
+++ Tarea1_1PM02/Tarea1_1PM02/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1_1PM02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void btndividir_Clicked(object sender, EventArgs e)
+        {
+                var num = new Models.Numeros
+        {
+                Result = Convert.ToInt32(txtnumero1.Text) / Convert.ToInt32(txtnumero2.Text)
+
+        };
+                var page = new Views.SegundaPagina();
+                page.BindingContext = num;
+                await Navigation.PushAsync(page);
+        }
+
+        private async void btnsuma_Clicked(object sender, EventArgs e)
+        {
+            var num = new Models.Numeros
+            {
+                Result = Convert.ToInt32(txtnumero1.Text) + Convert.ToInt32(txtnumero2.Text)
+
+            };
+            var page = new Views.SegundaPagina();
+            page.BindingContext = num;
+            await Navigation.PushAsync(page);
+        }
+
+        private async void btnresta_Clicked(object sender, EventArgs e)
+        {
+            var num = new Models.Numeros
+            {
+                Result = Convert.ToInt32(txtnumero1.Text) - Convert.ToInt32(txtnumero2.Text)
+
+            };
+            var page = new Views.SegundaPagina();
+            page.BindingContext = num;
+            await Navigation.PushAsync(page);
+        }
+
+        private async void btnmulti_Clicked(object sender, EventArgs e)
+        {
+            var num = new Models.Numeros
+            {
+                Result = Convert.ToInt32(txtnumero1.Text) * Convert.ToInt32(txtnumero2.Text)
+
+            };
+            var page = new Views.SegundaPagina();
+            page.BindingContext = num;
+            await Navigation.PushAsync(page);
+        }
+    }
+}
---
+++ Tarea1_1PM02/Tarea1_1PM02/Models/Numeros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1_1PM02.Models
+{
+    public class Numeros
+    {
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+        public int Result { get; set; }
+
+    }
+}
